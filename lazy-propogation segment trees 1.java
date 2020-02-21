4f19aa6d95162ac634606588f1cbca67f0de7038
Jatin <53038248+kaushikjatin@users.noreply.github.com>
2020-02-21 17:14:05 +0530

    Update lazy-propogation segment trees 1.java

--- lazy-propogation segment trees 1.java
+++ lazy-propogation segment trees 1.java
@@ -8,7 +8,7 @@ class Main
 		int n=scan.nextInt();
 		int value[]=new int[n];
 		int tree[]=new int[4*n+1];
-		int lazy[]=new int[n];
+		int lazy[]=new int[4*n+1];
 		for(int i=0;i<n;i++)
 		{
 		    value[i]=scan.nextInt();
@@ -20,9 +20,14 @@ class Main
 		    System.out.print(tree[i]+" ");
 		}
 		System.out.println();
-		int lazy[]=new int[4*n+1];
-		update_lazy(tree,0,n-1,0,2,4,10,lazy);
-		
+		update_lazy(tree,lazy,0,n-1,0,2,4,10);
+		int min=query_lazy(tree,lazy,0,n-1,0,4,5);
+		System.out.println(min);
+		for(int i=0;i<4*n+1;i++)
+		{
+		    System.out.print(tree[i]+" ");
+		}
+		System.out.println();
 	}
 	
 	// this fucntion is working fine........
@@ -45,18 +50,64 @@ class Main
 	}
 	
 	// this is an optimization of segment-trees ....
-	public static void update_lazy(int tree[],int se,int ee,int root,int l,int r,int increment,int lazy[])
+	public static void update_lazy(int tree[],int lazy[],int se,int ee,int root,int l,int r,int increment)
 	{
-	    // if it an out-of-bound.....
+	    
+	    // it means that we r out of bound............
 	    if(r<se || ee<l)
 	    {
-	        // here we will directly return because we need no change in this...it is just
-	        // out of the bound....
 	        return;
 	    }
 	    
 	    
-	    // firtly resolve the lazy value......
+	    // it means that we found a lazy value and hence need to be resolved.....
+	    if(lazy[root]!=0)
+	    {
+	        tree[root]+=lazy[root];
+	        if(se!=ee)//it means it is not a leaf node ans we need to pass the lazy value to the childrens....
+	        {
+	            lazy[2*root+1]=lazy[root];
+	            lazy[2*root+2]=lazy[root];
+	        }
+	        lazy[root]=0;
+	    }
+	    
+	    
+	    // now we will check for the complete overlap......
+	    if(l<=se && r>=ee)
+	    {
+	        tree[root]+=increment;
+	        if(se!=se)
+	        {
+	            lazy[2*root+1]+=increment;
+	            lazy[2*root+2]+=increment;
+	        }
+	        return;
+	    }
+	    
+	    
+	    
+	    // now only the case of partial-overlap is left
+	    update_lazy(tree,lazy,se,(se+ee)/2,2*root+1,l,r,increment);
+	    update_lazy(tree,lazy,(se+ee)/2 +1,ee,2*root+2,l,r,increment);
+	    tree[root]=find_min(tree[2*root+1],tree[2*root+2]);
+	    return;
+	    
+	    
+	    
+	}
+	
+	
+	public static int query_lazy(int tree[],int lazy[],int se,int ee,int root,int l,int r)
+	{
+	     // it means that we r out of bound............
+	    if(r<se || ee<l)
+	    {
+	        return 999999999;
+	    }
+	    
+	    
+	    // now if we found a lazy value
 	    if(lazy[root]!=0)
 	    {
 	        tree[root]+=lazy[root];
@@ -68,23 +119,20 @@ class Main
 	        lazy[root]=0;
 	    }
 	    
-	    // if it is a complete-overlap
-	    if(se>=l && ee<=r)
+	    
+	    // now suppose complete overlap...
+	    if(l<=se && r>=ee)
 	    {
-	        // now we just need to update value an then pass the lazy value to the roots..
-	        tree[root]+=increment;
-	        if(se!=ee)
-	        {
-	        lazy[2*root+1]=increment;
-	        lazy[2*root+2]=increment;
-	        }
-	        return;
+	        return tree[root];
 	    }
 	    
-	    
-	    // now if it is a partial-overlap...
+	    // now the case of partial-overlap...
+	    int min_left=query_lazy(tree,lazy,se,(se+ee)/2,2*root+1,l,r);
+	    int min_right=query_lazy(tree,lazy,(se+ee)/2 +1,ee,2*root+2,l,r);
+	    return find_min(min_left,min_right);
 	    
 	}
+	
 	public static int find_min(int x,int y)
 	{
 	    if(x<y)
