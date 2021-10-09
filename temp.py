c4db66aefdbb298a7995ce798fcb40ca2fb95d0a
Jatin Kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2021-10-09 06:25:53 +0530

    addded some notes

--- temp.py
+++ temp.py
@@ -1,52 +1,71 @@
-t = (5,'program', 1+3j)
-print(t)
+# t = (5,'program', 1+3j)
+# print(t)
 
-a = {1,2,2,3,3,3}
+# a = {1,2,2,3,3,3}
 
-print(a)
-print (type(a))
+# print(a)
+# print (type(a))
 
-def newFunction():
-    print("this is function")
+# def newFunction():
+#     print("this is function")
 
-d = { 1  : 'value' , 'key' : 2 }
+# d = { 1  : 'value' , 'key' : 2 }
 
-print(d)
-print (type(d))
+# print(d)
+# print (type(d))
 
 
-a = 1  + 2 + 3 + \
-	4 + 5 + 6 + \
-	7 + 8 + 9
+# a = 1  + 2 + 3 + \
+# 	4 + 5 + 6 + \
+# 	7 + 8 + 9
 
-print(a)
+# print(a)
 
-colors = ['red',
-          'blue',
-          'green']
-print(colors)
+# colors = ['red',
+#           'blue',
+#           'green']
+# print(colors) 
 
-a = 1; b = 2; c = 3
+# a = 1; b = 2; c = 3
 
           
 
-print(1,2,3,4,sep='*')  
+# print(1,2,3,4,sep='*')  
 
-print(1,2,3,4,sep='#',end='&')  
+# print(1,2,3,4,sep='#',end='&')  
 
-a=1
-b=2
-if a>b:
-    print(a)
-else:
-    print (b)
+# a=1
+# b=2
+# if a>b:
+#     print(a)
+# else:
+#     print (b)
 
 
-if a>b:
-    print(a)
-elif a==0:
-    print("none")
-else:
-    print (b)
-a=newFunction()
-print(a)
+# if a>b:
+#     print(a)
+# elif a==0:
+#     print("none")
+# else:
+#     print (b)
+# a=newFunction()
+# print(a)
+
+
+
+# using 5 different functions on the list
+# def temp1():
+#     return 2;
+# x=[2,3,4,[3,"abs"]];
+# ans=map(temp1,x)
+# print(list(ans))
+
+
+# x={2,3,4,5,1};
+# print(x)
+
+a, b, c, d, e, f = 0, 5, 12, 0, 15, 15
+exp1 = a <= b < c > d is not e is f
+exp2 = a is d > f is not c
+print(exp1)
+print(exp2)
